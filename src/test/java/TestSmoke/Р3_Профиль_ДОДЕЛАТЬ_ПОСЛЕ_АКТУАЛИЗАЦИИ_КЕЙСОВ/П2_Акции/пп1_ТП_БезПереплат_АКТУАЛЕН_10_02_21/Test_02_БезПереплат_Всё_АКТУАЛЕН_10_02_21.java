9bcb7ff06916604cd81912cd335c8c9dbd35ed67
Mikhail <mikhailnt@bk.ru>
2021-02-10 16:34:53 +0300

    28 актуализирую кейсы Акции

---
+++ src/test/java/TestSmoke/Р3_Профиль_ДОДЕЛАТЬ_ПОСЛЕ_АКТУАЛИЗАЦИИ_КЕЙСОВ/П2_Акции/пп1_ТП_БезПереплат_АКТУАЛЕН_10_02_21/Test_02_БезПереплат_Всё_АКТУАЛЕН_10_02_21.java
@@ -0,0 +1,37 @@
+package TestSmoke.Р3_Профиль_ДОДЕЛАТЬ_ПОСЛЕ_АКТУАЛИЗАЦИИ_КЕЙСОВ.П2_Акции.пп1_ТП_БезПереплат_АКТУАЛЕН_10_02_21;
+
+import base.TestBase;
+import io.qameta.allure.*;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class Test_02_БезПереплат_Всё_АКТУАЛЕН_10_02_21 extends TestBase {
+    @Epic(value = "Smoke MFTV Desktop Web")
+    @Feature(value = "3. Профиль")
+    @Story(value = "2. Акции")
+    @DisplayName(value ="БезПереплат.Всё")
+    @Severity(SeverityLevel.BLOCKER)
+    @Test
+
+    public void bundlBezPereplatMax() throws InterruptedException {
+        headerMenu.goToNilPage();
+        flowRegistation();
+        pageCMS.chooseBundleBezPereplatVseFromMsisdn("79260192144");
+        headerMenu.openSubsectionPromo();
+        promoPage.checkBundleBezPereplatVse();
+        pageCMS.deleteAccount();
+    }
+    private void flowRegistation() {
+        headerMenu.checkNotLoggedIsCorrect();
+        headerMenu.clickToEnter("Вход");
+        headerMenu.checkOpenFrameInputPhone();
+        headerMenu.inputLogin("9260192144");
+        headerMenu.clickToNext("Далее");
+        headerMenu.checkOpenFrameCreatePassword("9260192144", "111111");
+        headerMenu.clickToComeIn("Войти");
+        headerMenu.checkOpenFrameInputCod();
+        pageCMS.copyPasteCodMsisdn("79260192144");
+        headerMenu.clickToComeIn("Войти");
+        headerMenu.checkLoginUserIsCorrect();
+    }
+}
