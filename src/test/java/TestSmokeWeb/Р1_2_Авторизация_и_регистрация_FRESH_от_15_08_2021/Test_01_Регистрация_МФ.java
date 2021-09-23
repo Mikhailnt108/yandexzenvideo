05bfa119df3c78cda64ed118ffd0f44050852e14
Mikhail <mikhailnt@bk.ru>
2021-09-23 21:39:44 +0300

    #158 исправление commits git

--- src/test/java/TestSmokeWeb/Р1_2_Авторизация_и_регистрация_FRESH_от_15_08_2021/Test_01_Регистрация_МФ.java
+++ src/test/java/TestSmokeWeb/Р1_2_Авторизация_и_регистрация_FRESH_от_15_08_2021/Test_01_Регистрация_МФ.java
@@ -28,6 +28,7 @@ public class Test_01_Регистрация_МФ extends TestBaseWebDriver {
         flowRegistation();
         pageCMS.deleteAccountMF("79260192144");
     }
+
     private void flowRegistation() {
         headerMenu.checkNotLoggedIsCorrect();
         headerMenu.clickToEnter("Вход");
