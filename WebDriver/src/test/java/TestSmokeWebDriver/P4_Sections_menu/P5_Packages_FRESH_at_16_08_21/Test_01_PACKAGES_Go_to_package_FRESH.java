06ea8338f267568edc8309eaf2fe9c125e1f7ead
Mikhail <mikhailnt@bk.ru>
2021-10-09 00:01:22 +0300

    #281 Gitlab-CI запуск Allure report

--- WebDriver/src/test/java/TestSmokeWebDriver/P4_Sections_menu/P5_Packages_FRESH_at_16_08_21/Test_01_PACKAGES_Go_to_package_FRESH.java
+++ WebDriver/src/test/java/TestSmokeWebDriver/P4_Sections_menu/P5_Packages_FRESH_at_16_08_21/Test_01_PACKAGES_Go_to_package_FRESH.java
@@ -8,11 +8,11 @@ import org.junit.jupiter.api.Test;
 
 //@ResourceLock(value = "Suite#2", mode = ResourceAccessMode.READ_WRITE)
 public class Test_01_PACKAGES_Go_to_package_FRESH extends TestBaseWebDriver {
-//    @Epic(value = "Smoke MFTV Desktop Web")
-//    @Feature(value = "2. Разделы меню")
-//    @Story(value = "7. Пакеты")
-//    @DisplayName(value ="Переход в пакет")
-//    @Severity(SeverityLevel.BLOCKER)
+    @Epic(value = "Smoke MFTV Desktop Web")
+    @Feature(value = "2. Разделы меню")
+    @Story(value = "7. Пакеты")
+    @DisplayName(value ="Переход в пакет")
+    @Severity(SeverityLevel.BLOCKER)
     @Test
     @Tag("SuiteWdTest1")
     public void goToPackage() throws InterruptedException {
--- WebPlaywright/src/test/java/Test_08_2_TV_Efir_SCREENSHOT_VRT_PW.java
+++ WebPlaywright/src/test/java/Test_08_2_TV_Efir_SCREENSHOT_VRT_PW.java
@@ -3,15 +3,11 @@ import io.qameta.allure.*;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.parallel.Execution;
-import org.junit.jupiter.api.parallel.ExecutionMode;
-import org.junit.jupiter.api.parallel.ResourceAccessMode;
-import org.junit.jupiter.api.parallel.ResourceLock;
 
 import java.io.IOException;
 
-@Execution(ExecutionMode.SAME_THREAD)
-@ResourceLock(value = "SuitePW", mode = ResourceAccessMode.READ_WRITE)
+//@Execution(ExecutionMode.SAME_THREAD)
+//@ResourceLock(value = "SuitePW", mode = ResourceAccessMode.READ_WRITE)
 public class Test_08_2_TV_Efir_SCREENSHOT_VRT_PW extends TestBasePlaywright {
     @Epic(value = "Smoke MFTV Desktop Web")
     @Feature(value = "0. Разделы меню")
