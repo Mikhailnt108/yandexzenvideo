1c8fca3f0027982f1c0acd02c63eb104d3091b34
Mikhail <mikhailnt@bk.ru>
2021-11-12 02:09:47 +0300

    #382 запуск теста WD в контейнере с Allure report в CI

--- WebDriver/src/test/java/TestSmokeWebDriver/S1_Sections_menu/SS2_Films/Test_05_FILMS_Аренда_фильма_за_деньги.java
+++ WebDriver/src/test/java/TestSmokeWebDriver/S1_Sections_menu/SS2_Films/Test_05_FILMS_Аренда_фильма_за_деньги.java
@@ -13,7 +13,7 @@ import org.junit.jupiter.api.parallel.ResourceLock;
 //@Execution(ExecutionMode.SAME_THREAD)
 @ResourceLock(value = "SuiteWD#1", mode = ResourceAccessMode.READ_WRITE)
 public class Test_05_FILMS_Аренда_фильма_за_деньги extends TestBaseWebDriver {
-    @Epic(value = "Smoke MFTV Desktop Web")
+    @Epic(value = "smoke-mftv-desktop-web")
     @Feature(value = "2. Разделы меню")
     @Story(value = "3. Фильмы")
     @DisplayName(value ="Аренда фильма за деньги")
