ba51135be9a0116d93e32f9a1edad67cd7606cfc
Mikhail <mikhailnt@bk.ru>
2022-01-12 13:00:57 +0300

    #453 конфиг для запуска на ВМ тестов mWeb

--- mWebPlaywright/src/main/java/base/TestBasePlaywright.java
+++ mWebPlaywright/src/main/java/base/TestBasePlaywright.java
@@ -46,8 +46,8 @@ public class TestBasePlaywright extends BasePagePlaywright{
     public PreconditionPW preconditionPW;
     public AuthPagePW authPagePW;
     public PromoCodePW promoCodePW;
-    public String frontend = "https://web-preprod6.megafon.tv/";
-    public String backend = "https://bmp-preprod6.megafon.tv/";
+    public String frontend = "https://web-preprod1.megafon.tv/";
+    public String backend = "https://bmp-preprod1.megafon.tv/";
     public String numberBankCard = "4847 0000 6602 5312";
     public String dataValidity = "12 / 25";
     public String codeCVV = "258";
